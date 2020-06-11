be787ab39b484c49c859fedbd93b9a9344c9352a
JisooJang <ppuagirls@naver.com>
2020-06-12 00:47:32 +0900

    feat: add test codes for using functional interface in Collection framework (자바의 정석 14장 : 람다와 스트림)

--- src/functionalinterface/MyFunction.java
+++ src/functionalinterface/MyFunction.java
@@ -4,3 +4,9 @@ package functionalinterface;
 public interface MyFunction {
     void myMethod();
 }
+
+// 두 개 이상의 매개변수를 갖는 함수형 인터페이스가 필요하면 아래와 같이 직접 만들어 써야함.
+@FunctionalInterface
+interface TriFunction<T, U, V, R> {
+    R apply(T t, U u, V v);
+}
--- src/functionalinterface/Test.java
+++ src/functionalinterface/Test.java
@@ -1,10 +1,51 @@
 package functionalinterface;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
+
 public class Test {
     public static void main(String[] args) {
         Outer outer = new Outer();
         Outer.Inner inner = outer.new Inner();
         inner.method(100);
+
+        Predicate<String> isEmptyStr = s -> s.length() == 0;
+        String s = "";
+        if(isEmptyStr.test(s)) System.out.println("This is an empty String.");
+
+        // 매개변수가 2개인 함수형 인터페이스는 앞에 접두사 "Bi"가 붙는다.
+        BiConsumer<String, String> biConsumer = (s1, s2) -> System.out.println(s1 + s2);
+        BiPredicate<String, String> biPredicate = (s1, s2) -> s1.equals(s2);
+        BiFunction<String, String, Integer> biFunction = (s1, s2) -> s1.length() + s2.length();
+
+        // Collection Framework의 함수형 인터페이스
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 0 ; i < 10 ; i++) {
+            list.add(i+1);
+        }
+
+        list.forEach(i -> System.out.print(i + " ")); // 각 요소 출력 (모든 요소에 action을 수행)
+        System.out.println();
+
+        list.replaceAll(i -> i *= 10); // 각 요소를 변환(10을 곱함)하여 대체
+        System.out.println(list.toString());
+
+        list.removeIf(x -> x % 2 == 0 || x % 3 == 0); // list에서 조건(2 또는 3의 배수)에 맞는 요소들을 제거한다.
+        System.out.println(list);
+
+        Map<String, String> map = new HashMap<>();
+        map.put("1", "1");
+        map.put("2", "2");
+        map.put("3", "3");
+        map.put("4", "4");
+
+        map.forEach((k,v) -> System.out.println(k + ":" + v));
+
     }
 }
 
