24d2240a8243bcc5980f46958b07b64fe7b6ae40
JisooJang <ppuagirls@naver.com>
2020-06-14 00:31:11 +0900

    feat: add join() method in MultiThreadTest class

--- src/thread/MultiThreadTest.java
+++ src/thread/MultiThreadTest.java
@@ -4,13 +4,16 @@ public class MultiThreadTest {
     static long startTime = 0;
     public static void main(String[] args) {
         Thread t1 = new Thread(new ThreadTest());
-        t1.start();
-        startTime = System.currentTimeMillis();
+        Thread t2 = new Thread(new ThreadTest2());
 
-        for(int i=0 ; i<300 ; i++) {
-            System.out.printf("%s", new String("|"));
-        }
-        System.out.println("소요시간 : " + (System.currentTimeMillis() - MultiThreadTest.startTime));
+        t1.start();
+        t2.start();
+        startTime = System.currentTimeMillis();
+        try {
+            t1.join(); // main 쓰레드가 t1의 작업이 끝날때까지 기다림
+            t2.join(); // main 쓰레드가 t2의 작업이 끝날때까지 기다림
+        } catch(InterruptedException e) {}
+
     }
 }
 
@@ -18,6 +21,15 @@ class ThreadTest implements Runnable {
     public void run() {
         for(int i=0 ; i<300 ; i++) {
             System.out.printf("%s", new String("-"));
+        }
+        System.out.println("소요시간1 : " + (System.currentTimeMillis() - MultiThreadTest.startTime));
+    }
+}
+
+class ThreadTest2 implements Runnable {
+    public void run() {
+        for(int i=0 ; i<300 ; i++) {
+            System.out.printf("%s", new String("|"));
         }
         System.out.println("소요시간2 : " + (System.currentTimeMillis() - MultiThreadTest.startTime));
     }
